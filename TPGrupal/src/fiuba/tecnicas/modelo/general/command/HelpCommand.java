8de129ba4eee175c6df88ea06de2130191400b3e
jimena.soledad.tapia <jimena.soledad.tapia@7dc695ef-d1bf-dfec-0ed6-45268d579d43>
2013-05-26 21:25:37 +0000

    Help Command

---
+++ TPGrupal/src/fiuba/tecnicas/modelo/general/command/HelpCommand.java
@@ -0,0 +1,14 @@
+package fiuba.tecnicas.modelo.general.command;
+
+import fiuba.tecnicas.modelo.comun.Mensaje;
+import fiuba.tecnicas.modelo.comun.Resultado;
+
+import fiuba.tecnicas.modelo.general.Compra;
+
+public class HelpCommand implements ICommand {
+
+	@Override
+	public Resultado execute(String input) {
+		return new Resultado(Mensaje.getMensaje("mensaje_Help"));
+	}
+}
