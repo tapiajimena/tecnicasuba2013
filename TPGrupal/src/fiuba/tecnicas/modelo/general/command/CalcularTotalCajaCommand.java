3744402bb069a7ecb140f1f7839e0586a7076a7c
jimena.soledad.tapia <jimena.soledad.tapia@7dc695ef-d1bf-dfec-0ed6-45268d579d43>
2013-05-27 20:44:54 +0000

    calcularTotalCompraCommand

---
+++ TPGrupal/src/fiuba/tecnicas/modelo/general/command/CalcularTotalCajaCommand.java
@@ -0,0 +1,17 @@
+package fiuba.tecnicas.modelo.general.command;
+
+import fiuba.tecnicas.modelo.comun.Resultado;
+
+import fiuba.tecnicas.modelo.general.Compra;
+
+public class CalcularTotalCajaCommand implements ICommand {
+
+	/*
+	 * TODO Este lo hace Lucas.
+	 */
+	@Override
+	public Resultado execute(String input) {
+		String stringCosto = new Double(compra.CalcularTotal()).toString();
+		return new Resultado("La Compra tiene un Costo de" + stringCosto);
+	}
+}
